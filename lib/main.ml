63358638407a5329383ed193d52e4761ed9260da
zuodu1@laptop <dustin04dd@gmail.com>
2024-11-14 15:53:50 -0500

    11-14

--- lib/main.ml
+++ lib/main.ml
@@ -1,5 +1,20 @@
-(* source code here *)
+(* defining operation as the 4 common operators *)
+type op =
+  | Plus
+  | Minus
+  | Multiply
+  | Divide
 
-let rec fact n =
-    if n = 0 then 1
-    else n * fact (n - 1)
+(* exp as a constant or as a binary operation *)
+type exp =
+  | Const of int
+  | Binary of exp * op * exp
+
+(* computing expressions, !! divide by 0 undefined *)
+let apply (op : op) (x : float) (y : float) : float option = match op with
+  | Plus -> Some (x +. y)
+  | Minus -> Some (x -. y)
+  | Multiply -> Some (x *. y)
+  | Divide when y = 0.0 -> None
+  | Divide -> Some (x /. y)
+
